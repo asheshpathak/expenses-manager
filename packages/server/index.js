ba23af09ededc2f48a4e2b5ec18ef72b48ae18ba
Ashesh Pathak <ashesh.cosmid@gmail.com>
2024-05-01 02:41:00 +0530

    changed PORT

--- packages/server/index.js
+++ packages/server/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
