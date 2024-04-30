8491c740d403b11108ef598c5db4e13a57409fbe
Ashesh Pathak <ashesh.cosmid@gmail.com>
2024-05-01 03:24:02 +0530

    changes to the server

--- packages/server/index.js
+++ packages/server/index.js
@@ -9,9 +9,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, "../client", "build")));
 
 // Route all other requests to the React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+// });
+
+app.get("/",(req,res)=>{
+  res.send("Hello From Server")
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
